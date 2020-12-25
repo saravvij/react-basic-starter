246ac3e64b9fc6fb2147462902176126d8d63bd3
Saravanan Vijayappan <saravanan.vijayappan@anaplan.com>
2020-12-24 18:23:11 -0800

    Remove index.css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react'
-import './app.css';
+import React from 'react';
+import './style.css';
 
 function App() {
   return (
-    <div>
-      <h1>Hello!</h1>
+    <div className='app'>
+      <h1>Hello React!</h1>
+      <h2>Start editing to see some magic happen!</h2>
     </div>
   );
 }
